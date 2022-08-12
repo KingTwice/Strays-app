eebb18fbf4c32c06462b365d0bb97600566ead23
Alejandro Trejo <alejandro.trejo@enroutesystems.com>
2022-08-11 22:59:54 -0500

    Added Backend Project

---
+++ backend/src/main/java/com/trxjster/strays/StraysApplication.java
@@ -0,0 +1,13 @@
+package com.trxjster.strays;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StraysApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StraysApplication.class, args);
+    }
+
+}
---
+++ backend/src/test/java/com/trxjster/strays/StraysApplicationTests.java
@@ -0,0 +1,13 @@
+package com.trxjster.strays;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StraysApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
